3ff1314072014ae03460850118f4a3ab60669450
Kishore Koppula <kkishore906@gmail.com>
2025-09-16 14:06:27 +1000

    made changes to code

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React App Deployment to AWS</h1>
       <div className="card">
         <h3>About AWS</h3>
         <p>
