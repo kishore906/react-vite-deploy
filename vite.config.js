c837e4cc19b1e4da709191b2b481bacf56b74b16
Kishore Koppula <kkishore906@gmail.com>
2025-09-16 13:36:58 +1000

    changes nade in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-vite-deploy/",
   build: {
     outDir: "dist", // change output folder name here
   },
